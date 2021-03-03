a273de8e8f21c377f08e3de982900de7e6ca673b
Maximilian Wehrstedt <wehrstedt.coding@outlook.de>
2021-03-03 17:16:13 +0100

    new icons and fixed theme color for pwa

--- packages/gui/quasar.conf.js
+++ packages/gui/quasar.conf.js
@@ -133,7 +133,7 @@ module.exports = configure(function () {
         display: "standalone",
         orientation: "portrait",
         background_color: "#ffffff",
-        theme_color: "#027be3",
+        theme_color: "#303443",
         icons: [
           {
             src: "icons/icon-128x128.png",
